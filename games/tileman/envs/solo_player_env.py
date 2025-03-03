9b58e93de963872c98ee9bccdd080ab7fc2e7c79
left <107749872+0xleft@users.noreply.github.com>
2025-03-03 22:04:44 +0100

    lots more stuffs

--- games/tileman/envs/solo_player_env.py
+++ games/tileman/envs/solo_player_env.py
@@ -5,10 +5,10 @@ from .objects import Direction, Grid, Player, Tile, Vector, Game, Directions
 import pygame
 
 class SoloPlayerEnv(gymnasium.Env):
-    metadata = {"render_modes": ["human"]}
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
 
-    def __init__(self, grid_size=10, vision_range=5, max_steps=200, render_mode="human"):
+    def __init__(self, grid_size=10, vision_range=5, max_steps=200, render_mode="rgb_array"):
         super(SoloPlayerEnv, self).__init__()
         self.render_mode = render_mode
 
@@ -18,12 +18,11 @@ class SoloPlayerEnv(gymnasium.Env):
         self.game = Game(self.grid_size, self.grid_size)
         self.player = self.game.spawn_random_player()
 
-        pygame.init()
-        self.width = 500
-        self.height = 500
+        self.width = 600
+        self.height = 600
         self.grid_tile_size = min(self.width // len(self.game.grid.tiles[0]), self.height // len(self.game.grid.tiles))
-        self.screen = pygame.display.set_mode((self.width, self.height))
-
+        self.screen = None
+        self.clock = None
 
         self.action_space = spaces.Discrete(4)
         # The observation will be the coordinate of the agent
@@ -58,6 +57,9 @@ class SoloPlayerEnv(gymnasium.Env):
 
         # Optionally we can pass additional info, we are not using that for now
         info = {}
+        
+        if self.render_mode == "human":
+            self._render_frame()
 
         return (
             np.array([self.player.get_vision(self.game.grid, self.vision_range)]).astype(np.int8),
@@ -68,23 +70,49 @@ class SoloPlayerEnv(gymnasium.Env):
         )
 
     def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+    
+    def _render_frame(self):
+        if self.screen is None:
+            pygame.init()
+            if self.render_mode == "human":
+                pygame.display.init()
+                self.screen = pygame.display.set_mode(
+                    (self.width, self.height)
+                )
+            else:  # mode == "rgb_array"
+                self.screen = pygame.Surface((self.width, self.height))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+        
+        canvas = pygame.Surface((self.width, self.height))
+        canvas.fill((255, 255, 255))
+        
+        for y, row in enumerate(self.game.grid.tiles):
+            for x, tile in enumerate(row):
+                pygame.draw.rect(canvas, (0, 0, 0), (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size))
+                if tile.claimed:
+                    pygame.draw.rect(canvas, tile.claimer.color, (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size))
+                else:
+                    pygame.draw.rect(canvas, (20, 20, 20), (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size), 1)
+                
+                if tile.ocupied:
+                    margin = self.grid_tile_size // 5
+                    pygame.draw.rect(canvas, (max(tile.ocupant.color.r - 40, 0), max(tile.ocupant.color.g - 40, 0), max(tile.ocupant.color.b - 40, 0)), (x * self.grid_tile_size + margin, y * self.grid_tile_size + margin, self.grid_tile_size - 2 * margin, self.grid_tile_size - 2 * margin))
+        
+        self.surf = pygame.transform.flip(canvas, False, True)
+        self.screen.blit(self.surf, (0, 0))
         if self.render_mode == "human":
-            self.screen.fill((0, 0, 0))
-            for y, row in enumerate(self.game.grid.tiles):
-                for x, tile in enumerate(row):
-                    pygame.draw.rect(self.screen, (0, 0, 0), (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size))
-                    
-                    if tile.claimed:
-                        pygame.draw.rect(self.screen, tile.claimer.color, (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size))
-                    else:
-                        pygame.draw.rect(self.screen, (20, 20, 20), (x * self.grid_tile_size, y * self.grid_tile_size, self.grid_tile_size, self.grid_tile_size), 1)
-                    
-                    if tile.ocupied:
-                        margin = self.grid_tile_size // 5
-                        pygame.draw.rect(self.screen, (max(tile.ocupant.color.r - 40, 0), max(tile.ocupant.color.g - 40, 0), max(tile.ocupant.color.b - 40, 0)), (x * self.grid_tile_size + margin, y * self.grid_tile_size + margin, self.grid_tile_size - 2 * margin, self.grid_tile_size - 2 * margin))
-            
-
+            pygame.event.pump()
+            self.clock.tick(self.metadata["render_fps"])
             pygame.display.flip()
-
+        elif self.render_mode == "rgb_array":
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
+            )
+        
     def close(self):
-        pass
+        if self.screen is not None:
+            pygame.display.quit()
+            pygame.quit()
