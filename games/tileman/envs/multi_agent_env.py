f48d4805ea492bedf8de2378a5f9a6a236a12d3e
left <107749872+0xleft@users.noreply.github.com>
2025-03-05 11:55:33 +0100

    strat multi implementation

--- games/tileman/envs/multi_agent_env.py
+++ games/tileman/envs/multi_agent_env.py
@@ -7,14 +7,17 @@ import pygame
 import asyncio
 import websockets
 import pickle
+import multiprocessing
 
 class TileServer:
-    def __init__(self, host='0.0.0.0', port=9909):
+    def __init__(self, grid_size=40, vision_range=5, host='0.0.0.0', port=9909):
         self.host = host
         self.port = port
+        self.grid_size = grid_size
+        self.vision_range = vision_range
         self.clients = {}
-        self.game = Game(10, 10)
         self.players_moved = {}
+        self.game = Game(grid_size, grid_size)
 
     async def handler(self, websocket, path):
         player = self.game.spawn_random_player()
@@ -35,7 +38,33 @@ class TileServer:
             self.close()
             return
 
-        
+        print(action) # todo remove debug
+        player = self.clients[websocket]
+        player.move_direction = Directions[action]
+
+        self.players_moved[websocket] = True
+        if all(self.players_moved.values()):
+            self.game.update()
+            self.players_moved = {ws: False for ws in self.players_moved}
+
+            # send the new obs back
+            # todo
+            await asyncio.wait(websocket.send())
+            
+
+    async def send_observations(self):
+        data = {ws: (
+            np.array([self.clients[ws].get_vision(self.game.grid, self.vision_range)]).astype(np.int8),
+            reward,
+            terminated,
+            truncated,
+            info,
+        ) for ws in self.clients}
+        pickled_data = {
+            ws: pickle.dumps(data[ws])
+        for ws in self.clients}
+        await asyncio.wait([ws.send(pickled_data[ws]) for ws in self.clients.keys()])
+        self.moves.clear()
 
     def start(self):
         self.loop = asyncio.get_event_loop()
@@ -48,8 +77,10 @@ class TileServer:
             self.loop.stop()
 
     @staticmethod
-    def create_server():
-        pass
+    def create_server(grid_size=40, vision_range=5, host='0.0.0.0', port=9909):
+        # create new process
+        server = TileServer(grid_size, vision_range, host, port)
+        process = multiprocessing.Process()
 
 class SoloPlayerEnv(gymnasium.Env):
     pass
